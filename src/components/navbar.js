101d6405ce1ea9d5bead4509d8a531c3d161189d
Rithy <huotrithy01@gmail.com>
2023-10-22 09:34:06 +0700

    Set up react and create navbar components

---
+++ src/components/navbar.js
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>Blog Site</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">New Blog</a>
+            </div>
+        </nav>
+    );
+} 
+export default Navbar;
