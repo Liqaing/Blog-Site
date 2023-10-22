9d5172805898a3945b78015cc5894eaaafcdcd60
Rithy <huotrithy01@gmail.com>
2023-10-22 09:41:50 +0700

    Create home componenets

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Navbar from './components/navbar';
+import Home from './components/home';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <div className="content">
+        <Home/>  
+      </div>
     </div>
   );
 }
