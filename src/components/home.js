c92cc0def35924d0c50ccf6fb885cef6065c385e
Rithy <huotrithy01@gmail.com>
2023-11-05 22:16:09 +0700

    Add error handling when request data
    To display error message to user in case of failing to get data from server

--- src/components/home.js
+++ src/components/home.js
@@ -6,24 +6,38 @@ const Home = () => {
     // Use state hook
     const [blogs, setBlogs] = useState(null);
     const [isLoading, setLoading] = useState(true); // Initialy loading is true
+    const [error, setError] = useState(null);
 
     // Use useEffect hook to request data from API endpoint
     // useEffect run when component render to page
     useEffect(() => {
         // Fetch data from api endpoint and set data to blogs state
         fetch('http://localhost:8000/blogs')
-        .then(res => res.json())
-        .then(data => {
-            setBlogs(data);
-            setLoading(false) // When request is succeed, loading message is not longer need
-        })
+            .then(res => {
+                if (!res.ok) {
+                    throw Error("Unable to load the blog");
+                }
+                return res.json();
+            })
+            .then(data => {
+                setBlogs(data);
+                setLoading(false); // When request is succeed, loading message is not longer need
+                setError(null);
+            })
+            .catch(err => {
+                setError(err.message);
+                setLoading(false);
+                setBlogs(null);
+            })
     });
 
     return ( 
         <div className="home">
-            {/* Call BlogList component and provide blogs state as prop to render 
-                Check if blogs is not null before pass it into component to render */}
+            
+            {error && <div className="error">{ error }</div>}
             {isLoading && <div>Loading...</div>}
+
+            {/* Check if blogs is null, if not then call BlogList component and provide blogs state as prop to render */}
             {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
         </div>
     );
