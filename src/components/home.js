8b82282fecc806f0be8dfefff118f6e7f2dd62c4
Rithy <huotrithy01@gmail.com>
2023-11-05 15:57:42 +0700

    Request data from api endpoint to display
    Use useEffect hook to request data from api and display to user

--- src/components/home.js
+++ src/components/home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./bloglist";
 
 const Home = () => { 
@@ -6,10 +6,22 @@ const Home = () => {
     // Use state hook
     const [blogs, setBlogs] = useState(null);
 
+    // Use useEffect hook to request data from API endpoint
+    // useEffect run when component render to page
+    useEffect(() => {
+        // Fetch data from api endpoint and set data to blogs state
+        fetch('http://localhost:8000/blogs')
+        .then(res => res.json())
+        .then(data => {
+            setBlogs(data);
+        })
+    });
+
     return ( 
         <div className="home">
-            {/* Call BlogList component and provide blogs state as prop to render */}
-            <BlogList blogs={blogs} title="All Blogs"/>
+            {/* Call BlogList component and provide blogs state as prop to render 
+                Check if blogs is not null before pass it into component to render */}
+            {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
         </div>
     );
 }
