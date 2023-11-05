553a370825bc4632953a8b3cdc70a58eadc2e040
Rithy <huotrithy01@gmail.com>
2023-11-05 13:22:25 +0700

    Remove delete function

--- src/components/bloglist.js
+++ src/components/bloglist.js
@@ -1,5 +1,5 @@
 // Take in the prop which provided by parent compenent 
-const BlogList = ({blogs, title, handleDeleteBlog}) => {
+const BlogList = ({blogs, title}) => {
     return ( 
         <div className="blog-list">
             <h2>{title}</h2>
@@ -8,7 +8,6 @@ const BlogList = ({blogs, title, handleDeleteBlog}) => {
                 <div className="blog-preview" key={blog.id}>
                     <h3>{ blog.title }</h3>
                     <p> written by { blog.author }</p>
-                    <button onClick={() => handleDeleteBlog(blog.id)}>Delete Blog</button>
                 </div>
             ))}   
         </div>
--- src/components/home.js
+++ src/components/home.js
@@ -6,26 +6,10 @@ const Home = () => {
     // Use state hook
     const [blogs, setBlogs] = useState(null);
 
-    // Delete blog by id
-    const handleDeleteBlog = (id) => {
-        // Create new array to store blog that are now not match the id
-        // If the id doesn't match, it is not the blog to delete
-        const newBlogs = blogs.filter((blog) => blog.id !== id);
-
-        // Update blogs state to newBlogs
-        setBlogs(newBlogs);
-    }
-
     return ( 
         <div className="home">
-            {/* Call BlogList component and provide blogs state, function as prop to render */}
-            <BlogList blogs={blogs} title="All Blogs" handleDeleteBlog={handleDeleteBlog}/>
-            
-            <BlogList blogs={blogs.filter((blog) => {
-                // Return only the blogs written by Rick into the components
-                return blog.author === 'Rick';
-            })} title="Rick's Blogs" handleDeleteBlog={handleDeleteBlog}/>
-
+            {/* Call BlogList component and provide blogs state as prop to render */}
+            <BlogList blogs={blogs} title="All Blogs"/>
         </div>
     );
 }
