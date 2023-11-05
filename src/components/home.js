fb0165bb7d658207b1b23d9838c5996cc7a6d65e
Rithy <huotrithy01@gmail.com>
2023-11-05 13:20:16 +0700

    Remove data pass in when first create useState

--- src/components/home.js
+++ src/components/home.js
@@ -3,12 +3,8 @@ import BlogList from "./bloglist";
 
 const Home = () => { 
 
-    // Use state
-    const [blogs, setBlogs] = useState([
-        {title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1},
-        {title: 'Welcome to the party', body: 'lorem ipsum...', author: 'Rick', id: 2},
-        {title: 'Programming Language', body: 'lorem ipsum...', author: 'Devit', id: 3},
-    ]);
+    // Use state hook
+    const [blogs, setBlogs] = useState(null);
 
     // Delete blog by id
     const handleDeleteBlog = (id) => {
