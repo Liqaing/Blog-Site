2a6fa76015cccb6bb0cfeeedba9c0af14c838850
Rithy <huotrithy01@gmail.com>
2023-11-05 16:13:02 +0700

    Display loading message when still request data
    Display loading to show user that data is still loading

--- src/components/home.js
+++ src/components/home.js
@@ -5,6 +5,7 @@ const Home = () => {
 
     // Use state hook
     const [blogs, setBlogs] = useState(null);
+    const [isLoading, setLoading] = useState(true); // Initialy loading is true
 
     // Use useEffect hook to request data from API endpoint
     // useEffect run when component render to page
@@ -14,6 +15,7 @@ const Home = () => {
         .then(res => res.json())
         .then(data => {
             setBlogs(data);
+            setLoading(false) // When request is succeed, loading message is not longer need
         })
     });
 
@@ -21,6 +23,7 @@ const Home = () => {
         <div className="home">
             {/* Call BlogList component and provide blogs state as prop to render 
                 Check if blogs is not null before pass it into component to render */}
+            {isLoading && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
         </div>
     );
