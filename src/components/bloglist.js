b76d3f127634e8fd66a161bae543a9638f29afcf
Rithy <huotrithy01@gmail.com>
2023-11-12 20:40:53 +0700

    Surround preview blog with link map to blog detail

--- src/components/bloglist.js
+++ src/components/bloglist.js
@@ -1,14 +1,18 @@
+import { Link } from "react-router-dom";
+
 // Take in the prop which provided by parent compenent 
 const BlogList = ({blogs, title}) => {
     return ( 
         <div className="blog-list">
             <h2>{title}</h2>
-            
+
             {/* Loop through each blog in blogs and return a html */}
             {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
-                    <h3>{ blog.title }</h3>
-                    <p> written by { blog.author }</p>
+                    <Link to={`/blogs/${blog.id}`}>
+                        <h3>{ blog.title }</h3>
+                        <p> written by { blog.author }</p>
+                    </Link>
                 </div>
             ))}   
         </div>
