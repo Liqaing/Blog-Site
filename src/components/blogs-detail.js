63401cb23973aec43abaab9d0cc3bf55a89954b6
Rithy <huotrithy01@gmail.com>
2023-11-13 23:58:26 +0700

    Handle delete blog

--- src/components/blogs-detail.js
+++ src/components/blogs-detail.js
@@ -1,12 +1,23 @@
-import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { useHistory, useParams } from "react-router-dom/cjs/react-router-dom.min";
 import useFetch from "../customHooks/useFetch";
 
 const BlogDetails = () => {
 
     // Use params hook is used to retrive dynamic value that pass through the url
     const { id } = useParams();
+    const history = useHistory();
     
     const {data: blog, isLoading, error} = useFetch(`http://localhost:8000/blogs/${id}`);
+
+    const handleDelete = () => {
+        fetch(`http://localhost:8000/blogs/${id}`, {
+            method: 'DELETE',
+        })
+        .then(() => {
+            console.log('Blog deleted');
+            history.push('/');
+        })
+    }
 
     return (
         <div className="blog-details">
@@ -19,6 +30,7 @@ const BlogDetails = () => {
                     <div>
                         { blog.body }
                     </div>
+                    <button onClick={handleDelete}>Delete</button>
                 </article>
             )}
         </div>
