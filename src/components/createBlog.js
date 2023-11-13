cf4b7e54c02e12900c40371f8a00c8a01221549e
Rithy <huotrithy01@gmail.com>
2023-11-13 22:22:43 +0700

    Create add blog form, style and keep track of form
    Create add form blog and use state hook to keep track of the value type in and update state
    apply style to form

--- src/components/createBlog.js
+++ src/components/createBlog.js
@@ -1,7 +1,42 @@
+import { useState } from "react";
+
 const CreateBlog = () => {
+
+    // Create state to keep track of form input
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('Choose an author');
+
     return (
         <div className="create-blog">
             <h2>Create new blog</h2>
+            <form>
+                <label>Blog title</label>
+                <input 
+                    type="text"
+                    // Set title to value of state
+                    value={title}
+                    // Update value of state when change
+                    onChange={(e) => setTitle(e.target.value)}
+                    required 
+                />
+                <label>Blog content</label>
+                <textarea
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
+                    required
+                ></textarea>
+                <label>Author</label>
+                <select
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                >
+                    <option value="Choose an author" disabled>Choose an author</option>
+                    <option value="Rick">Rick</option>
+                    <option value="Mario">Mario</option>
+                </select>
+                <button>Add blog</button>
+            </form>
         </div>
     );
 }
